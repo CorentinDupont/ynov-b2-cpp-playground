be9f2e7c3caf252ed14aec5d08530e737963224d
CorentinDupont <dupont.corentin44@gmail.com>
2018-02-12 16:42:56 +0100

    hackerrank exo 5

--- CorentinDupont/main.cpp
+++ CorentinDupont/main.cpp
@@ -65,3 +65,35 @@ void hr_4_ConditionalStatement(){
         cout << "Greater than 9" << endl;
     }
 }
+
+void hr_5_ForLoop(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    for(int n=a;n<=b;n++){
+        if(n == 1){
+            cout << "one" << endl;
+        }else if(n == 2){
+            cout << "two" << endl;
+        }else if(n == 3){
+            cout << "three" << endl;
+        }else if(n == 4){
+            cout << "four" << endl;
+        }else if(n == 5){
+            cout << "five" << endl;
+        }else if(n == 6){
+            cout << "six" << endl;
+        }else if(n == 7){
+            cout << "seven" << endl;
+        }else if(n == 8){
+            cout << "eight" << endl;
+        }else if(n == 9){
+            cout << "nine" << endl;
+        }else if(n > 9){
+            if(n%2==0){
+                cout << "even" << endl;
+            }else{
+                cout << "odd" << endl;
+            }
+        }
+    }
+}
