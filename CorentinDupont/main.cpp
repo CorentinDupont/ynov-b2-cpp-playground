a4b9f9e35264a29f6b07cb81de9d1bb22982fbd8
CorentinDupont <dupont.corentin44@gmail.com>
2018-02-12 14:46:45 +0100

    commit initial

---
+++ CorentinDupont/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void hrInputAndOutput();
+
+int main() {
+    hrInputAndOutput();
+    return 0;
+}
+
+//Hacker rank exercises
+void hrInputAndOutput(){
+    int a, b, c;
+    cout << "Veuillez entrer trois nombres entre 0 et 1000 pour en faire l'addition" << endl;
+
+
+
+}
+
