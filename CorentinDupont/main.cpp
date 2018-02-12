2497de19e63b6dc0f3fb81bacc3f6d1947585b02
CorentinDupont <dupont.corentin44@gmail.com>
2018-02-12 15:33:04 +0100

    hackerrank exo 2

--- CorentinDupont/main.cpp
+++ CorentinDupont/main.cpp
@@ -5,19 +5,28 @@
 #include <algorithm>
 using namespace std;
 
-void hrInputAndOutput();
+void hr_2_InputAndOutput();
 
 int main() {
-    hrInputAndOutput();
+    hr_2_InputAndOutput();
     return 0;
 }
 
 //Hacker rank exercises
-void hrInputAndOutput(){
-    int a, b, c;
-    cout << "Veuillez entrer trois nombres entre 0 et 1000 pour en faire l'addition" << endl;
-
-
-
+void hr_2_InputAndOutput(){
+    int a = 0, b = 0, c = 0;
+    int sum;
+    bool cinIsFine = false;
+    while (!cinIsFine){
+        cout << "Veuillez entrer trois nombres entre 0 et 1000 pour en faire l'addition" << endl;
+        cin >> a >> b >> c;
+        if( a<0 || a>1000 || b<0 || b>1000 || c<0|| c>1000){
+            cout << "Un ou plusieurs nombre(s) ne respecte(nt) pas les conditions" << endl;
+        }else{
+            cinIsFine = true;
+        }
+    }
+    sum = a+b+c;
+    cout << sum << endl;
 }
 
